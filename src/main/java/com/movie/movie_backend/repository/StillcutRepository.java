1002e9ac7d0d0592924aab34d19c6076235d2000
kimgyuhyun <kgh980605@gmail.com>
2025-06-29 13:53:45 +0900

    Add new repository and migration files

---
+++ src/main/java/com/movie/movie_backend/repository/StillcutRepository.java
@@ -0,0 +1,10 @@
+package com.movie.movie_backend.repository;
+
+import com.movie.movie_backend.entity.Stillcut;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface StillcutRepository extends JpaRepository<Stillcut, Long> {
+    // 기본 CRUD 메서드만 사용
+} 
