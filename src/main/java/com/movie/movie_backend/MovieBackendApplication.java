806aa9535bc2639c9862e14025de4047cc42863b
KimGyuhyun <kgh980605@gmail.com>
2025-06-30 10:07:09 +0900

    feat: movie management initial setup

--- src/main/java/com/movie/movie_backend/MovieBackendApplication.java
+++ src/main/java/com/movie/movie_backend/MovieBackendApplication.java
@@ -6,7 +6,7 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableScheduling
-public class MovieBackendApplication {
+public class  MovieBackendApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MovieBackendApplication.class, args);
